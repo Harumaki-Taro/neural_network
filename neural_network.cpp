a76f4ef412cd6725b8fd3e21db24e0fe66fefd80
Harumaki Taro <coffee.git@gmail.com>
2018-02-11 14:32:06 +0900

    neural networkクラスを作った

--- neural_network.cpp
+++ neural_network.cpp
@@ -68,23 +68,49 @@ vector<float> W3 {
 };
 
 
+class Neural_Network {
+    /*
+    */
+public:
+    vector<float> forwardprop(void);
+    void backprop(vector<float>);
+
+private:
+    vector<float> L1;
+    vector<float> L2;
+};
+
+vector<float> Neural_Network::forwardprop(void) {
+    L1 = sigmoid(dot(X, W1, 4, 4, 6));
+    L2 = sigmoid(dot(L1, W2, 4, 6, 3));
+    vector<float> pred = sigmoid(dot(L2, W3, 4, 3, 2));
+
+    return pred;
+}
+
+void Neural_Network::backprop(vector<float> pred) {
+    vector<float> pred_error = y - pred;
+    vector<float> W3_delta = pred_error * sigmoid_d(pred);
+    vector<float> W2_delta = dot(W3_delta, transpoose(W3, 3, 2), 4, 2, 3) * sigmoid_d(L2);
+    vector<float> W1_delta = dot(W2_delta, transpoose(W2, 6, 3), 4, 3, 6) * sigmoid_d(L1);
+    vector<float> dE_dW3 = dot(transpoose(L2, 4, 3), W3_delta, 3, 4, 2);
+    vector<float> dE_dW2 = dot(transpoose(L1, 4, 6), W2_delta, 6, 4, 3);
+    vector<float> dE_dW1 = dot(transpoose(X, 4, 4), W1_delta, 4, 4, 6);
+    W3 = W3 + dE_dW3;
+    W2 = W2 + dE_dW2;
+    W1 = W1 + dE_dW1;
+}
+
+
 
 int main(int argc, const char * argv[]) {
 
+    Neural_Network nn;
+    vector<float> pred;
+
 	for (unsigned int i = 0; i != 1000; ++i) {
-		vector<float> L1 = sigmoid(dot(X, W1, 4, 4, 6));
-		vector<float> L2 = sigmoid(dot(L1, W2, 4, 6, 3));
-        vector<float> pred = sigmoid(dot(L2, W3, 4, 3, 2));
-		vector<float> pred_error = y - pred;
-		vector<float> W3_delta = pred_error * sigmoid_d(pred);
-        vector<float> W2_delta = dot(W3_delta, transpoose(W3, 3, 2), 4, 2, 3) * sigmoid_d(L2);
-		vector<float> W1_delta = dot(W2_delta, transpoose(W2, 6, 3), 4, 3, 6) * sigmoid_d(L1);
-        vector<float> dE_dW3 = dot(transpoose(L2, 4, 3), W3_delta, 3, 4, 2);
-		vector<float> dE_dW2 = dot(transpoose(L1, 4, 6), W2_delta, 6, 4, 3);
-		vector<float> dE_dW1 = dot(transpoose(X, 4, 4), W1_delta, 4, 4, 6);
-        W3 = W3 + dE_dW3;
-		W2 = W2 + dE_dW2;
-		W1 = W1 + dE_dW1;
+        pred = nn.forwardprop();
+        nn.backprop(pred);
 
 		if ( i ==  999) {
 			print(pred, 4, 2);
