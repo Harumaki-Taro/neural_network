3105b68ae7ecdad5a0877f087120e293156606db
Harumaki Taro <coffee.git@gmail.com>
2018-03-12 07:36:54 +0900

    使用スレッド数の自動決定

--- openmp.h
+++ openmp.h
@@ -1,13 +1,27 @@
 #ifndef INCLUDE_openmp_h_
 #define INCLUDE_openmp_h_
 
+#include <iostream>
 #include <omp.h>
 
 void use_openmp(void) {
     // openMP
     int n = Eigen::nbThreads( );
-    omp_set_num_threads(n-2);
-    Eigen::setNbThreads(n-2);
+    if ( n > 3 ) {
+        n -= 2;
+    } else if ( n == 3 ) {
+        n = 2;
+    } else if ( n == 2 ) {
+        n = 1;
+    } else if ( n == 1 ) {
+        std::cout << "このデバイスはシングルスレッドです。OpenMPは使えません。" << std::endl;
+        exit(1);
+    } else {
+        std::cout << "スレッド数が異常です。" << std::endl;
+        exit(1);
+    }
+    omp_set_num_threads(n);
+    Eigen::setNbThreads(n);
 }
 
 
